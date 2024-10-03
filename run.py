feb6cd13ef034fd9abc95fd71bdceef0f4dd5100
Harley Scott <harley.scott@students.codeinstitute.net>
2024-10-03 12:39:37 +0000

    added validation to check for exactly 6 values which the user inputs

--- run.py
+++ run.py
@@ -18,11 +18,29 @@ def get_attendance_data():
     """
     Get attendance numbers from the users
     """
-print("Please enter attendance data from the last market.")
-print("Data should be six numbers, seperated by commas.")
-print("Example: 1, 2, 3, 4, 5, 6\n")
+    print("Please enter attendance data from the last market.")
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 1, 2, 3, 4, 5, 6\n")
 
-data_str = input("Enter your data here: ")
-print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ")
+    
+    attendance_data = data_str.split(",")
+    validate_data(attendance_data)
+
+
+def validate_data(values):
+    """
+    converts all string values into integers.
+    Raises valueerror if strings cannot be converted into integers,
+    or if they are not precisely 6 numbers.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Precisely 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Data is invalid: {e}, please try again.\n")
+
 
 get_attendance_data()
